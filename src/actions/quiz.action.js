fe3c19113cc692362278134a48af127b8785501a
mhndkptr <muhamadhendika77@gmail.com>
2025-12-14 02:56:56 +0700

    feat: integration quiz backoffice

--- src/actions/quiz.action.js
+++ src/actions/quiz.action.js
@@ -2,6 +2,15 @@
 
 import request, { handleAxiosError } from '@/utils/baseRequest'
 
+export async function getQuizByIdAction({ id }) {
+  try {
+    const res = await request.get(`/quiz/${id}`)
+    return res.data
+  } catch (error) {
+    return handleAxiosError(error)
+  }
+}
+
 export async function createQuizAction({ body }) {
   try {
     const res = await request.post('/quiz', body)
@@ -20,9 +29,36 @@ export async function updateQuizAction({ id, body }) {
   }
 }
 
+export async function updateQuizQuestionAction({ id, body }) {
+  try {
+    const res = await request.patch(`/quiz/question/${id}`, body)
+    return res.data
+  } catch (error) {
+    return handleAxiosError(error)
+  }
+}
+
 export async function deleteQuizAction({ id }) {
   try {
     const res = await request.delete(`/quiz/${id}`)
+    return res.data
+  } catch (error) {
+    return handleAxiosError(error)
+  }
+}
+
+export async function deleteQuizQuestionAction({ id }) {
+  try {
+    const res = await request.delete(`/quiz/question/${id}`)
+    return res.data
+  } catch (error) {
+    return handleAxiosError(error)
+  }
+}
+
+export async function createQuizQuestionAction({ body }) {
+  try {
+    const res = await request.post('/quiz/question', body)
     return res.data
   } catch (error) {
     return handleAxiosError(error)
